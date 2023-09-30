18572e56017d4f817c17dbe4441633b03067040f
simontopliss <simontopliss@me.com>
2023-09-30 23:23:37 +0100

    New tests

---
+++ PawTrackerTests/FeatureTests/AppTests.swift
@@ -0,0 +1,9 @@
+@testable import PawTracker
+import XCTest
+
+@MainActor
+final class AppTests: XCTestCase {
+    func test_zero() throws {
+        XCTFail("Tests not yet implemented in AppTests")
+    }
+}
